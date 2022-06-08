4f96cce94aa62698e62161beaec3b5af8418fbc8
Rohan Patel <rohanpatel4846@gmail.com>
2022-06-09 00:50:30 +0530

    Added FoldersFiles API

--- BreadCrumb/src/app/Services/LocalBaseService/local-base-custom.service.ts
+++ BreadCrumb/src/app/Services/LocalBaseService/local-base-custom.service.ts
@@ -13,7 +13,7 @@ export class LocalBaseCustomService {
   db:any;
 
   constructor(public FoldersFiles: FoldersFilesService) { 
-    this.db = new Localbase('db');
+    this.db = new Localbase('BreadCrumbDB');
   }
 
   getFoldersFilesFromGoogleSheet(){
@@ -25,10 +25,54 @@ export class LocalBaseCustomService {
       
       this.FoldersFiles.getAllData(body)
       .subscribe((response:any) => {
-        this.db.collection('FoldersFiles').add(response).then((result:any) => {
-          observer.next(true);
+        console.log(response);
+        if(response.status == "200"){
+          let foldersCompleted = 0;
+          this.db.collection('Folders').delete().then((result:any) => {
+            response.data.forEach((res:any) => {
+              this.db.collection('Folders').add({
+                data: res.Data.Folders,
+                ServerId: res.ServerID
+              }).then((result:any) => {
+                foldersCompleted++;
+                if(foldersCompleted >= response.data.length){
+                  let filesCompleted = 0;
+                  this.db.collection('Files').delete().then((result:any) => {
+                    response.data.forEach((res:any) => {
+                      this.db.collection('Files').add({
+                        data: res.Data.Files,
+                        ServerId: res.ServerID
+                      }).then((result:any) => {
+                        filesCompleted++;
+                        if(filesCompleted >= response.data.length){
+                          let fileLinksCompleted = 0;
+                          this.db.collection('File_Links').delete().then((result:any) => {
+                            response.data.forEach((res:any) => {
+                              this.db.collection('File_Links').add({
+                                data: res.Data.File_Links,
+                                ServerId: res.ServerID
+                              }).then((result:any) => {
+                                fileLinksCompleted++;
+                                if(fileLinksCompleted >= response.data.length){
+                                  observer.next(true);
+                                  observer.complete();
+                                }
+                              });
+                            });
+                          });
+                        }
+                      });
+                    });
+                  });
+                }
+              });
+            });
+          });
+        }
+        else{
+          observer.next(false);
           observer.complete();
-        });
+        }
       },
       (error) => {
         observer.next(false);
@@ -40,7 +84,7 @@ export class LocalBaseCustomService {
 
   getFoldersFilesFromLocalBase(){
     let finalData = new Observable((observer:any) => {
-      this.db.collection('FoldersFiles').get().then((result:any) => {
+      this.db.collection('Folders').get().then((result:any) => {
         observer.next(result);
         observer.complete();
       })
