877fd3c6b70bd8a1d9dd27f2f110e7afa3e5a8e6
Rohan Patel <42444158+gamble4846@users.noreply.github.com>
2022-06-17 00:13:34 +0530

    .

--- BreadCrumb/src/app/Components/menu/menu.component.ts
+++ BreadCrumb/src/app/Components/menu/menu.component.ts
@@ -16,11 +16,13 @@ export class MenuComponent implements OnInit {
   }
 
   MenuLinkClicked(route:any, pageName:any){
-    this.router.navigate(
-      [route]
-    );
-    this.currentPage = pageName;
     this.menuOpen = !this.menuOpen;
+    setTimeout(() => {
+      this.router.navigate(
+        [route]
+      );
+      this.currentPage = pageName;
+    }, 500);
   }
 
   ToggleMenu(){
