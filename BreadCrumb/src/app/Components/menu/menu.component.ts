f2b90624bb513a18963c1dd99d706b677078938c
Apurva20997 <96464493+Apurva20997@users.noreply.github.com>
2022-06-16 16:13:07 +0530

    Menu Completed

--- BreadCrumb/src/app/Components/menu/menu.component.ts
+++ BreadCrumb/src/app/Components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  menuOpen:boolean = false;
+  currentPage:String = "Home";
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  MenuLinkClicked(route:any, pageName:any){
+    this.router.navigate(
+      [route]
+    );
+    this.currentPage = pageName;
+  }
+
+  ToggleMenu(){
+    this.menuOpen = !this.menuOpen;
+  }
 }
