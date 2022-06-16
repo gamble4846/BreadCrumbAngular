9511860bb7c25b692e58833443828bddc6d35de6
Apurva20997 <96464493+Apurva20997@users.noreply.github.com>
2022-06-16 18:11:08 +0530

    .

--- BreadCrumb/src/app/Components/menu/menu.component.ts
+++ BreadCrumb/src/app/Components/menu/menu.component.ts
@@ -16,6 +16,7 @@ export class MenuComponent implements OnInit {
   }
 
   MenuLinkClicked(route:any, pageName:any){
+    this.menuOpen = !this.menuOpen;
     this.router.navigate(
       [route]
     );
